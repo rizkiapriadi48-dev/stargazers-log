09cdb2ee5bca88d53b238ed6ac618f48a733c454
rizkiapriadi48-dev <rizkiapriadi48@gmail.com>
2026-08-15 06:12:50 +0700

    Add recipe object for Rendang with ingredients and steps

---
+++ recipe.js
@@ -0,0 +1,26 @@
+const recipe = {
+    nama : "Rendang",
+    Kategori : "Makanan",
+
+    bahan : [
+        "1_ 1 kg daging sapi (pilih bagian paha/gandik agar tidak mudah hancur)", 
+        "2_ 1.000 ml santan kental (dari 2–3 butir kelapa tua)",  
+        "3_ 1.000 ml santan encer", 
+        "4_ 1 lembar daun kunyit (ikat simpul)", 
+        "5_ 3 lembar daun jeruk (buang tulang daunnya)", 
+        "6_ 2 batang serai (memarkan)", 
+        "7_ 2 buah asam kandis (bisa diganti 1 sdt air asam jawa)",
+        "8_ 1 lembar daun salam", 
+        "9_ Garam secukupnya",
+    ],
+     langkah : [
+        "1_ Persiapan Daging", 
+        "2_ Memasak Kuah Bumbu", 
+        "3_ Masukkan Daging", 
+        "4_ Proses Pengentalan (Kalio)", 
+        "5_ Proses Pengeringan (Rendang)",
+        "6_ Siap disajikan" 
+
+     ]
+}
+console.log(recipe);
